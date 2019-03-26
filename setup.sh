7cc495db32165a53f870c7bba4ef19f9072feea5
Jiayuan Ren <renjiayuan1314@gmail.com>
2019-03-25 18:28:31 -0700

    run the script local, easy to debug.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,5 @@
 
 # setup the meld.
 mkdir -p ~/.local/share/nautilus/scripts
-curl https://raw.githubusercontent.com/renjiayuan1314/UbuntuDesktopEnvSetup/master/meld/compare-to-left.sh -o ~/.local/share/nautilus/scripts/compare-to-left.sh
-curl https://raw.githubusercontent.com/renjiayuan1314/UbuntuDesktopEnvSetup/master/meld/select-as-left.sh -o ~/.local/share/nautilus/scripts/select-as-left.sh
-curl https://raw.githubusercontent.com/renjiayuan1314/UbuntuDesktopEnvSetup/master/meld/compare.sh -o ~/.local/share/nautilus/scripts/compare.sh
+cp ./meld/*.sh ~/.local/share/nautilus/scripts/.
 chmod 777 ~/.local/share/nautilus/scripts/*.sh
