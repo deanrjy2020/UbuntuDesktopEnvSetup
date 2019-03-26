38e01d4efe2ec4a04ae3ab3c80cb198d2eec5535
Jiayuan Ren <renjiayuan1314@gmail.com>
2019-03-25 18:22:09 -0700

    fix meld bug

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 # setup the meld.
 mkdir -p ~/.local/share/nautilus/scripts
-curl https://raw.githubusercontent.com/renjiayuan1314/UbuntuDesktopEnvSetup/master/meld/*.sh -o ~/.local/share/nautilus/scripts/.
+curl https://raw.githubusercontent.com/renjiayuan1314/UbuntuDesktopEnvSetup/master/meld/compare-to-left.sh -o ~/.local/share/nautilus/scripts/compare-to-left.sh
+curl https://raw.githubusercontent.com/renjiayuan1314/UbuntuDesktopEnvSetup/master/meld/select-as-left.sh -o ~/.local/share/nautilus/scripts/select-as-left.sh
+curl https://raw.githubusercontent.com/renjiayuan1314/UbuntuDesktopEnvSetup/master/meld/compare.sh -o ~/.local/share/nautilus/scripts/compare.sh
 chmod 777 ~/.local/share/nautilus/scripts/*.sh
